da2bb5f3e78d37b6946c7d34e3c144c9901f5d8b
Guido <gbassi@frba.utn.edu.ar>
2025-04-05 11:58:39 -0300

    parte logs hecha, falta programar cliente servidor

--- client/main.go
+++ client/main.go
@@ -3,16 +3,27 @@ package main
 import (
 	"client/globals"
 	"client/utils"
+	"log"
 )
 
 func main() {
 	utils.ConfigurarLogger()
 
+	log.Println("Hola soy un log")  // log. funciones de log
+
 	// loggear "Hola soy un log" usando la biblioteca log
 	globals.ClientConfig = utils.IniciarConfiguracion("config.json")
+
 	// validar que la config este cargada correctamente
+	if globals.ClientConfig == nil {
+		log.Fatal("Error al cargar la configuracion")
+	}
 
 	// loggeamos el valor de la config
+	log.Printf("Configuracion cargada: %+v", globals.ClientConfig)
+
+	utils.LeerConsola() // leer de la consola el mensaje
+
 
 	// ADVERTENCIA: Antes de continuar, tenemos que asegurarnos que el servidor esté corriendo para poder conectarnos a él
 
