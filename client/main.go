e7cba312ddc6f80cd87f0962ff0b84ebc83becbe
Guido <gbassi@frba.utn.edu.ar>
2025-04-05 13:02:23 -0300

    creo que terminado

--- client/main.go
+++ client/main.go
@@ -22,16 +22,18 @@ func main() {
 	// loggeamos el valor de la config
 	log.Printf("Configuracion cargada: %+v", globals.ClientConfig)
 
-	utils.LeerConsola() // leer de la consola el mensaje
+	//utils.LeerConsola() // leer de la consola el mensaje
 
 
 	// ADVERTENCIA: Antes de continuar, tenemos que asegurarnos que el servidor esté corriendo para poder conectarnos a él
 
 	// enviar un mensaje al servidor con el valor de la config
 
+	utils.EnviarMensaje(globals.ClientConfig.Ip, globals.ClientConfig.Puerto,globals.ClientConfig.Mensaje)
+
 	// leer de la consola el mensaje
-	// utils.LeerConsola()
+	utils.LeerConsola()
 
 	// generamos un paquete y lo enviamos al servidor
-	// utils.GenerarYEnviarPaquete()
+	utils.GenerarYEnviarPaquete()
 }
